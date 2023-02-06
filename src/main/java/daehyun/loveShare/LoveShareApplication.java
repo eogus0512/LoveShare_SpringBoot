807498f61a898c77b8217feeb49cc112340655a4
eogus0512 <eogus0512@gmail.com>
2023-02-06 23:01:55 +0900

    clone

---
+++ src/main/java/daehyun/loveShare/LoveShareApplication.java
@@ -0,0 +1,13 @@
+package daehyun.loveShare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/daehyun/loveShare/LoveShareApplicationTests.java
@@ -0,0 +1,13 @@
+package daehyun.loveShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
