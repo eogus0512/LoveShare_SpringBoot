979966417eca3e0b39cc80ef0235100bca572974
eogus0512 <eogus0512@gmail.com>
2023-02-14 23:21:38 +0900

    로그인, 회원가입 구현

--- src/main/java/daehyun/loveShare/web/login/LoginController.java
+++ src/main/java/daehyun/loveShare/web/login/LoginController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
